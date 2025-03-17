fced4628609c4cb50fbf2d720ad9918bdf3418cc
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-03-17 22:44:56 +0700

    delete

---
+++ src/BlockBuillderReward.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.27;
+import {IContribution} from "./contribution/IContribution.sol";
+
+contract BlockBuilderReward {
+    IContribution public contribution;
+}
