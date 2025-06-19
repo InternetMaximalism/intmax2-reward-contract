14bad44f2a7b5a9c9c706b4dae7331225ed52ec5
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-06-19 21:57:00 +0700

    feat: improvement of minter & test scripts (#12)
    * feat: add transferTo
    
    * feat: fix
    
    * feat: more valiation on Minter
    
    * feat: impl deploy script for minter
    
    * feat: impl intmax token deploy script
    
    * feat: setup minter test
    
    * chore: minor fix
    
    * chore: fmt
    
    * feat: add sepolia config
    
    * chore: add sepolia settings

--- src/minter/Minter.sol
+++ src/minter/Minter.sol
@@ -5,18 +5,23 @@ import {IMinter} from "./IMinter.sol";
 import {IINTMAXToken} from "../token/mainnet/IINTMAXToken.sol";
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
 /**
  * @title Minter
- * @dev Contract responsible for minting INTMAX tokens and distributing them to liquidity
+ * @notice Contract responsible for minting INTMAX tokens and distributing them to liquidity
+ * @dev This contract uses role-based access control with TOKEN_MANAGER_ROLE for minting/distribution
+ *      and DEFAULT_ADMIN_ROLE for administrative functions. It implements UUPS upgradeability.
  * @custom:security-contact security@intmax.io
  */
-contract Minter is IMinter, OwnableUpgradeable, UUPSUpgradeable {
-    /// @dev Reference to the INTMAX token contract
+contract Minter is IMinter, AccessControlUpgradeable, UUPSUpgradeable {
+    /// @notice Role that grants permission to mint tokens and transfer to liquidity
+    bytes32 public constant TOKEN_MANAGER_ROLE = keccak256("TOKEN_MANAGER_ROLE");
+
+    /// @notice Reference to the INTMAX token contract
     IINTMAXToken public intmaxToken;
 
-    /// @dev Address where liquidity tokens will be sent
+    /// @notice Address where liquidity tokens will be sent
     address public liquidity;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -26,18 +31,21 @@ contract Minter is IMinter, OwnableUpgradeable, UUPSUpgradeable {
 
     /**
      * @notice Initializes the contract with required dependencies
-     * @dev Sets up the contract with the INTMAX token and liquidity address
+     * @dev Sets up the contract with the INTMAX token, liquidity address, and admin
      * @param _intmaxToken Address of the INTMAX token
      * @param _liquidity Address for liquidity distribution
+     * @param _admin Address that will be granted the DEFAULT_ADMIN_ROLE
      * @custom:oz-upgrades-init-compat initializer
      */
-    function initialize(address _intmaxToken, address _liquidity) external initializer {
-        if (_intmaxToken == address(0) || _liquidity == address(0)) {
+    function initialize(address _intmaxToken, address _liquidity, address _admin) external initializer {
+        if (_intmaxToken == address(0) || _liquidity == address(0) || _admin == address(0)) {
             revert AddressZero();
         }
 
-        __Ownable_init(_msgSender());
+        __AccessControl_init();
         __UUPSUpgradeable_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
 
         intmaxToken = IINTMAXToken(_intmaxToken);
         liquidity = _liquidity;
@@ -45,24 +53,86 @@ contract Minter is IMinter, OwnableUpgradeable, UUPSUpgradeable {
 
     /**
      * @notice Mints new INTMAX tokens to this contract
-     * @dev Only the contract owner can call this function
+     * @dev Only addresses with TOKEN_MANAGER_ROLE can call this function
      */
-    function mint() external onlyOwner {
+    function mint() external onlyRole(TOKEN_MANAGER_ROLE) {
+        uint256 balanceBefore = intmaxToken.balanceOf(address(this));
+
+        // Perform external call
         intmaxToken.mint(address(this));
+
+        uint256 balanceAfter = intmaxToken.balanceOf(address(this));
+
+        // Check for potential overflow/underflow and ensure balance increased
+        if (balanceAfter < balanceBefore) {
+            revert MintFailed();
+        }
+
+        uint256 mintedAmount = balanceAfter - balanceBefore;
+
+        // Ensure tokens were actually minted
+        if (mintedAmount == 0) {
+            revert NoTokensMinted();
+        }
+
+        emit Minted(mintedAmount);
     }
 
     /**
      * @notice Transfers tokens from this contract to the liquidity address
-     * @dev Only the contract owner can call this function
+     * @dev Only addresses with TOKEN_MANAGER_ROLE can call this function
      * @param amount The amount of tokens to transfer
      */
-    function transferToLiquidity(uint256 amount) external onlyOwner {
-        intmaxToken.transfer(liquidity, amount);
+    function transferToLiquidity(uint256 amount) external onlyRole(TOKEN_MANAGER_ROLE) {
+        if (amount == 0) {
+            revert ZeroAmount();
+        }
+
+        uint256 contractBalance = intmaxToken.balanceOf(address(this));
+        if (contractBalance < amount) {
+            revert InsufficientBalance();
+        }
+
+        bool success = intmaxToken.transfer(liquidity, amount);
+        if (!success) {
+            revert TransferFailed();
+        }
+
+        emit TransferredToLiquidity(amount);
     }
 
     /**
-     * @dev Function that authorizes an upgrade to a new implementation
+     * @notice Transfers tokens from this contract to the specified address
+     * @dev Only addresses with DEFAULT_ADMIN_ROLE can call this function
+     * @param to The address to transfer tokens to
+     * @param amount The amount of tokens to transfer
+     */
+    function transferTo(address to, uint256 amount) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        if (to == address(0)) {
+            revert ZeroRecipient();
+        }
+
+        if (amount == 0) {
+            revert ZeroAmount();
+        }
+
+        uint256 contractBalance = intmaxToken.balanceOf(address(this));
+        if (contractBalance < amount) {
+            revert InsufficientBalance();
+        }
+
+        bool success = intmaxToken.transfer(to, amount);
+        if (!success) {
+            revert TransferFailed();
+        }
+
+        emit TransferredTo(to, amount);
+    }
+
+    /**
+     * @notice Authorizes an upgrade to a new implementation
+     * @dev Only addresses with DEFAULT_ADMIN_ROLE can authorize upgrades
      * @param newImplementation Address of the new implementation
      */
-    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+    function _authorizeUpgrade(address newImplementation) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
 }
