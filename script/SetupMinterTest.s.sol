7357e9aa2ccae78761f205d79e20cd55f40fb05c
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-06-21 23:31:31 +0700

    fix: fixed setup script

--- script/SetupMinterTest.s.sol
+++ script/SetupMinterTest.s.sol
@@ -5,6 +5,8 @@ import {Script, console} from "forge-std/Script.sol";
 import {Minter} from "../src/minter/Minter.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {INTMAXToken} from "../src/token/mainnet/INTMAXToken.sol";
+import {DeployINTMAXToken} from "./DeployINTMAXToken.s.sol";
+import {DeployMinter} from "./DeployMinter.s.sol";
 
 /**
  * @title DeployMinter
@@ -13,22 +15,31 @@ import {INTMAXToken} from "../src/token/mainnet/INTMAXToken.sol";
  */
 contract SetupMinterTest is Script {
     function run() external {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         uint256 adminPrivateKey = vm.envUint("ADMIN_PRIVATE_KEY");
-
-        address token_address = vm.envAddress("INTMAX_TOKEN_ADDRESS");
-        address minter_address = vm.envAddress("MINTER_ADDRESS");
         address liquidity_address = vm.envAddress("LIQUIDITY_ADDRESS");
         address token_manager_address = vm.envAddress("TOKEN_MANAGER_ADDRESS");
 
+        // convert private key to address
+        address adminAddress = vm.addr(adminPrivateKey);
+
+        // deploy token contract
+        DeployINTMAXToken deployToken = new DeployINTMAXToken();
+        INTMAXToken intmaxToken = deployToken.deploy(deployerPrivateKey, adminAddress, address(0));
+
+        console.log("INTMAXToken deployed at:", address(intmaxToken));
+
+        // deploy minter contract
+        DeployMinter deployMinter = new DeployMinter();
+        Minter minter = deployMinter.deploy(deployerPrivateKey, address(intmaxToken), liquidity_address, adminAddress);
+        console.log("Minter deployed at:", address(minter));
+
+        // start setup with admin private key
         vm.startBroadcast(adminPrivateKey);
-
-        INTMAXToken intmaxToken = INTMAXToken(token_address);
-        Minter minter = Minter(minter_address);
-
         // grant minter role to the minter contract
-        if (!intmaxToken.hasRole(intmaxToken.MINTER_ROLE(), minter_address)) {
-            intmaxToken.grantRole(intmaxToken.MINTER_ROLE(), minter_address);
-            console.log("Granted MINTER_ROLE to:", minter_address);
+        if (!intmaxToken.hasRole(intmaxToken.MINTER_ROLE(), address(minter))) {
+            intmaxToken.grantRole(intmaxToken.MINTER_ROLE(), address(minter));
+            console.log("Granted MINTER_ROLE to:", address(minter));
         }
 
         // grant minter role to the liquidity address
